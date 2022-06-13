25baae8f19c9c3670a39d703f8f2caf9c3400480
Ryo Utsunomiya <millionsnowliving@gmail.com>
2022-06-13 23:49:29 +0900

    Add rainbow

---
+++ lib/rainbowable.rb
@@ -0,0 +1,14 @@
+module Rainbowable
+  def rainbow
+    to_s.each_char.map.with_index do |char, count|
+      color = 31 + count % 6
+      "\e[#{color}m#{char}"
+    end.join + "\e[0m"
+  end
+end
+
+# String.include Rainbowable
+# Array.include Rainbowable
+#
+# puts 'abc'.rainbow
+# puts [123, 456, 789].rainbow
